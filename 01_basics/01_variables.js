457f65d98f8144d47cdd7aede97f11f5ec777264
neeleshjawariya <neelesh.jawariya@gmail.com>
2025-12-09 04:41:26 +0000

    add new file datatype  and variable

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountID = 144424
+let accountEmail = "jawariya143@gmail.com"
+var accountPassword = "12345678"
+accountCity = "mukara"
+
+//accountId = 2 // not allowed
+
+
+accountEmail = "jawariya144"
+accountPassword = "11122212"
+accountCity = "bengaluru"
+
+console.log("accountID")
+console.table([accountID , accountEmail , accountPassword, accountCity])
