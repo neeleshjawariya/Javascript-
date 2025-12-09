277fccc850df093429e807c04d6f66ea28226bed
neeleshjawariya <neelesh.jawariya@gmail.com>
2025-12-09 06:41:35 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all js code as newer version
+
+//aleart( 3 + 3) // we are using nodejs ,not broser 
+
+console.log(3 + 3)  // code readability should be high
+
+
+
+console.log("neelesh")
+
+
+let name = "neelesh"
+let age = 18
+let isLoggedIn = false
+
+
+//numer => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbole
+
+
+//object 
+
+console.log(typeof "neelesh")
